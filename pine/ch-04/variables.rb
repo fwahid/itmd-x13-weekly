7c24314fb40d638d19fcdac1b331a9f23c4c9e53
fwahid <fwahid@hawk.iit.edu>
2017-05-31 11:49:16 -0500

    added other half of code

--- pine/ch-04/variables.rb
+++ pine/ch-04/variables.rb
@@ -11,3 +11,21 @@ name = 'Anya Christina Emmanuella Jenkins Harris'
 puts 'My name is ' + name + '.'
 puts 'Wow! ' + name
 puts 'is a really long name!'
+
+# Also, just as we can assign an object to a variable, we can reassign a
+# different object to that variable. (This is why we call them variables—
+# what they point to can vary.)
+
+# Example of reassining objects to that variable,..
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+# Variables can point to any kind of object, not just strings:
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+2)
+puts my_own_var
