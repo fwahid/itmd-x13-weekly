a3534072dcbdd58d5d3e33b8e0ac3fae0990aeee
fwahid <fwahid@hawk.iit.edu>
2017-05-31 11:47:42 -0500

    progress...

---
+++ pine/ch-04/variables.rb
@@ -0,0 +1,13 @@
+# variables.rb
+# Chapter 4 - Variables and Assignments
+# starts on page 19
+
+# Storing Variables aka "Assignments"
+my_string = '...You can say that again...'
+puts my_string
+puts my_string
+
+name = 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
