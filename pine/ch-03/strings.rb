2b2674d2a60502568d392633111c0f2060effa1f
fwahid <fwahid@hawk.iit.edu>
2017-05-31 11:45:07 -0500

    started strings.rb

---
+++ pine/ch-03/strings.rb
@@ -0,0 +1,17 @@
+# strings.rb
+# Chapter 3 - starts on page 14
+
+# Trying puts with strings
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# 3.1 String Arithmetic
+# adding strings
+puts 'I like' + 'apple pie.'
+# added space!!
+puts 'I like' + ' apple pie.'
+
+# you can multiply strings too
+puts 'blink ' * 4
+#blink blink blink blink
