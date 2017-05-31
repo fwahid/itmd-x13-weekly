c8ed5708e6f9804e0b48e405d9d58c73a04fb056
fwahid <fwahid@hawk.iit.edu>
2017-05-31 11:40:52 -0500

    finished

--- pine/ch-02/calc.rb
+++ pine/ch-02/calc.rb
@@ -11,3 +11,13 @@ puts 2.0 * 3.0
 puts 5.0 - 8.0
 puts 9.0 / 2.0
 
+# integers
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+# 'puts 9 / 2' will output '4' --  rounding down from 4.5 answer
+
+# final complex expressions
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
